e2c6f634f55232025f105ba889adf4870b25bd82
Elijah Insua <tmpvar@gmail.com>
2014-02-04 23:19:17 -0700

    first round implementation of segment2

--- segment2.js
+++ segment2.js
@@ -1,47 +1,122 @@
-function Segment2(start, end) {
+if (typeof require !== 'undefined') {
+  var Vec2 = require('vec2');
+  var segseg = require('segseg');
+}
 
+
+function Segment2(start, end) {
+  if (!(this instanceof Segment2)) {
+    return new Segment2(start, end);
+  }
+
+  this.start = new Vec2(start);
+  this.end = new Vec2(end);
+
+  this._listeners = [];
 }
 
 Segment2.prototype.change = function(fn) {
-
+  this._listeners.push(fn);
+  return fn;
 };
 
 Segment2.prototype.ignore = function(fn) {
 
+  if (!fn) {
+    this._listeners = [];
+  } else {
+    this._listeners = this._listeners.filter(function(a) {
+      return a !== fn;
+    });
+  }
+
 };
 
 Segment2.prototype.notify = function() {
-
+  var fns = this._listeners, l = fns.length;
+  for (var i=0; i<l; i++) {
+    fns[i](this);
+  }
 };
 
 Segment2.prototype.length = function() {
-
+  return this.start.distance(this.end);
 };
 
 Segment2.prototype.lengthSquared = function() {
-
+  return this.start.subtract(this.end).lengthSquared();
 };
 
 Segment2.prototype.closestPointTo = function(vec) {
+  var a = this.start;
+  var b = this.end;
+  var ab = b.subtract(a, true);
+  var veca = vec.subtract(a, true);
+  var vecadot = veca.dot(ab);
+  var abdot = ab.dot(ab);
 
+  var t = Math.min(Math.max(vecadot/abdot, 0), 1);
+
+  var point = ab.multiply(t).add(a);
+  var length = vec.subtract(point, true).lengthSquared();
+
+  return point;
 };
 
 Segment2.prototype.midpoint = function() {
-
+  return this.start.subtract(this.end, true).divide(2).add(this.end);
 };
 
-Segment2.prototype.intersect = function(line) {
+Segment2.prototype.slope = function() {
+  var dy = this.end.y - this.start.y;
+  var dx = this.end.x - this.start.x;
+  return Vec2.clean(dy/dx);
+};
 
+Segment2.prototype.rotate = function(rads, origin, returnNew) {
+
+  if (origin === true) {
+    returnNew = true;
+    origin = null;
+  }
+
+  if (!origin) {
+    origin = this.midpoint();
+  }
+
+  var start = this.start.subtract(origin, true).rotate(rads);//.add(origin);
+  var end = this.end.subtract(origin, true).rotate(rads);//.add(origin);
+
+  start.add(origin);
+  end.add(origin);
+
+  if (returnNew) {
+    return new Segment2(start, end);
+  } else {
+    this.start.set(start.x, start.y);
+    this.end.set(end.x, end.y);
+    return this;
+  }
+};
+
+Segment2.prototype.intersect = function(seg) {
+  if (!seg || !seg.start || !seg.end) {
+    return false;
+  }
+
+  var isect = segseg(this.start, this.end, seg.start, seg.end);
+  if (!isect || isect === true) {
+    return isect;
+  } else {
+    return Vec2.fromArray(isect);
+  }
 };
 
 if (typeof module !== "undefined" && typeof module.exports == "object") {
-  module.exports = segment2;
+  module.exports = Segment2;
 }
 
 if (typeof window !== "undefined") {
-  window.segment2 = window.segment2 || segment2;
+  window.Segment2 = window.Segment2 || Segment2;
 }
 
-module.exports = function() {
-
-}
--- test/test.js
+++ test/test.js
@@ -1,16 +1,190 @@
-var segment2;
+
 if (typeof require !== "undefined") {
-  segment2 = require("../segment2.js");
-} else {
-  segment2 = window.segment2;
+  var Segment2 = require("../segment2.js");
+  var Vec2 = require("vec2");
 }
 
 var ok = function(a, msg) { if (!a) throw new Error(msg || "not ok"); };
 
 describe('segment2', function() {
-  describe('#', function() {
-    it('', function() {
-      
+  describe('#change', function() {
+    it('should add a listener', function() {
+      var s = Segment2();
+
+      ok(s._listeners.length === 0);
+
+      s.change(function() {});
+      ok(s._listeners.length === 1);
+    });
+
+    it('should return the passed function', function() {
+      var s = Segment2();
+      var fn = function() {};
+      ok(s.change(fn) === fn);
     });
   });
+
+  describe('#ignore', function() {
+    it('it should ignore the passed function', function() {
+
+      var s = Segment2();
+      var fn1 = s.change(function() {});
+      var fn2 = s.change(function() {});
+
+      ok(s._listeners.length === 2);
+      s.ignore(fn1);
+      ok(s._listeners.length === 1);
+      ok(s._listeners[0] === fn2);
+    });
+
+    it('should ignore all if no function is passed', function() {
+      var s = Segment2();
+      var fn1 = s.change(function() {});
+      var fn2 = s.change(function() {});
+
+      s.ignore();
+      ok(s._listeners.length === 0);
+    });
+  });
+
+  describe('#notify', function() {
+    it('should call all of the listeners in the order of addition', function() {
+      var s = Segment2();
+      var calls = '';
+      var fn1 = s.change(function() { calls += '1'; });
+      var fn2 = s.change(function() { calls += '2'; });
+
+      s.notify();
+      ok(calls === '12');
+    });
+
+    it('should call the callback with itself as the first arg', function(t) {
+      var s = Segment2();
+      s.change(function(seg) {
+        ok(seg === s);
+        t();
+      });
+      s.notify();
+    });
+  });
+
+  describe('#length', function() {
+    it('it should compute the length of line segment', function() {
+      var s = new Segment2(new Vec2(0, 0), new Vec2(10, 0));
+      ok(s.length() === 10);
+    });
+  });
+
+  describe('#lengthSquared', function() {
+    it('it should compute the length of line segment', function() {
+      var s = new Segment2(new Vec2(0, 0), new Vec2(10, 0));
+      ok(s.lengthSquared() === 100);
+    });
+  });
+
+  describe('#closestPointTo', function() {
+    it('should return a vec2 that is the closest point on this line to the passed vec', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(10, 0));
+      ok(s.closestPointTo(Vec2(5, 10)).equal(Vec2(5, 0)))
+    });
+  });
+
+  describe('#midpoint', function() {
+    it('should return a vec2 equidistant to start and end', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(10, 0));
+      ok(s.midpoint().equal(Vec2(5, 0)));
+
+      var s2 = Segment2(Vec2(-10, 0), Vec2(10, 0));
+      ok(s2.midpoint().equal(Vec2(0, 0)));
+    });
+  });
+
+  describe('#slope', function() {
+    it('should returnt the slope of the segment 1/1', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(10, 10));
+      ok(s.slope() === 1);
+    });
+
+    it('should return the slope of the segment 2/1', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(10, 20));
+      ok(s.slope() === 2);
+    });
+
+    it('should return the slope of the segment 1/2', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(20, 10));
+      ok(s.slope() === .5);
+    });
+  });
+
+  describe('#intersect', function() {
+    it('should return true if colinear', function() {
+      var s = Segment2(Vec2(0,0), Vec2(10, 0));
+      var s2 = Segment2(Vec2(5,0), Vec2(10, 0));
+      ok(s.intersect(s2) === true);
+    });
+
+    it('should return a vec2 at intersection', function() {
+      var s = Segment2(Vec2(0,0), Vec2(10, 0));
+      var s2 = Segment2(Vec2(5,10), Vec2(5, -10));
+      ok(s.intersect(s2).equal(Vec2(5, 0)));
+    });
+
+    it('should return falsy if no intersection', function() {
+      var s = Segment2(Vec2(0,0), Vec2(10, 0));
+      var s2 = Segment2(Vec2(5,10), Vec2(5, 1));
+      ok(!s.intersect(s2));
+    });
+
+    it('should return falsy if invalid seg is passed', function() {
+      var s = Segment2(Vec2(0,0), Vec2(10, 0));
+      ok(!s.intersect(null));
+      ok(!s.intersect({}));
+      ok(!s.intersect({ start : {} }));
+    });
+  });
+
+  describe('#rotate', function() {
+    it('should choose the midpoint by default', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(10, 0));
+      s.rotate(Math.PI/2);
+      ok(s.start.equal(Vec2(5, -5)));
+      ok(s.end.equal(Vec2(5, 5)));
+    });
+
+    it('should choose the midpoint by default (null) (returnNew)', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(10, 0));
+      var s2 = s.rotate(Math.PI/2, null, true);
+      ok(s2.start.equal(Vec2(5, -5)));
+      ok(s2.end.equal(Vec2(5, 5)));
+      ok(s2 !== s);
+    });
+
+    it('should choose the midpoint by default (returnNew)', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(10, 0));
+      var s2 = s.rotate(Math.PI/2, true);
+      ok(s2.start.equal(Vec2(5, -5)));
+      ok(s2.end.equal(Vec2(5, 5)));
+      ok(s2 !== s);
+    });
+
+    it('should rotate around origin if passed', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(10, 0));
+      s.rotate(Math.PI, Vec2(5, 10));
+      ok(s.start.equal(Vec2(10, 20)));
+      ok(s.end.equal(Vec2(0, 20)));
+    });
+
+    it('should rotate around origin if passed (returnNew)', function() {
+      var s = Segment2(Vec2(0, 0), Vec2(10, 0));
+      var s2 = s.rotate(Math.PI, Vec2(5, 10), true);
+      ok(s2.start.equal(Vec2(10, 20)));
+      ok(s2.end.equal(Vec2(0, 20)));
+      ok(s2 !== s);
+    });
+
+
+
+
+  });
+
 });
